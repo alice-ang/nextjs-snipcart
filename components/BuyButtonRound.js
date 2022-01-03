b365bb8a86b682ff953f3eedf85e087e9b656b26
Alice Anglesjö <alice.anglesjo@hotmail.com>
2022-01-03 14:12:14 +0100

    fix buybutton

--- components/BuyButtonRound.js
+++ components/BuyButtonRound.js
@@ -4,18 +4,21 @@ import { BsFillCartPlusFill } from "react-icons/bs";
 import { Breakpoints } from "../styles/styles";
 
 const RoundButton = styled.button({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
   width: 30,
   height: 30,
   alignSelf: "end",
-  background: theme.colors.dark,
-  color: theme.colors.light,
+  background: theme.colors.light,
+  color: theme.colors.dark,
   borderRadius: 50,
   textAlign: "center",
   border: `2px solid ${theme.colors.dark}`,
   transition: "0.3s",
   "&:hover": {
-    background: theme.colors.light,
-    color: theme.colors.dark,
+    background: theme.colors.dark,
+    color: theme.colors.light,
   },
   [Breakpoints.XL]: {
     width: 40,
@@ -24,11 +27,11 @@ const RoundButton = styled.button({
 });
 
 const StyledCart = styled(BsFillCartPlusFill)({
-  width: 14,
-  height: 14,
+  width: 18,
+  height: 18,
   [Breakpoints.XL]: {
-    width: 18,
-    height: 18,
+    width: 20,
+    height: 20,
   },
 });
 
