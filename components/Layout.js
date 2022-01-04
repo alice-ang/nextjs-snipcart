ed9ae9c8785a3fd0006f259e9b136aa611017a22
Alice Anglesjö <alice.anglesjo@hotmail.com>
2022-01-04 22:26:49 +0100

    move back to layout

--- components/Layout.js
+++ components/Layout.js
@@ -22,11 +22,43 @@ export default function Layout({ children }) {
         />
         <link rel="shortcut icon" href="../public/favicon.ico" />
       </Head>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          var chatbox = document.getElementById('fb-customer-chat');
+          chatbox.setAttribute("page_id", "734860390568287");
+          chatbox.setAttribute("attribution", "biz_inbox");
+      
+          window.fbAsyncInit = function() {
+            FB.init({
+              appId            : '734860390568287',
+              autoLogAppEvents : true,
+              xfbml            : true,
+              version          : 'v12.0'
+            });
+          };
+        
+          (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = '<https://connect.facebook.net/sv_SE/sdk/xfbml.customerchat.js>';
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+            `,
+        }}
+      />
       <LayoutContainer>
         <Header />
         <Content>{children}</Content>
         <Footer />
       </LayoutContainer>
+      <script
+        async
+        defer
+        crossorigin="anonymous"
+        src="https://connect.facebook.net/sv_SE/sdk.js"
+      ></script>
       <script
         async
         src="https://cdn.snipcart.com/themes/v3.3.0/default/snipcart.js"
--- next.config.js
+++ next.config.js
@@ -8,31 +8,4 @@ module.exports = {
   images: {
     domains: ["cdn.sanity.io"],
   },
-  script: [
-    {
-      type: "text/javascript",
-      hid: "fb-customer-chat",
-      body: true,
-      innerHTML: `
-      var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "734860390568287");
-      chatbox.setAttribute("attribution", "biz_inbox");
-  
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v12.0'
-        });
-      };
-    
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = '<https://connect.facebook.net/sv_SE/sdk/xfbml.customerchat.js>';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));`,
-    },
-  ],
-  __dangerouslyDisableSanitizersByTagID: { "fb-customer-chat": ["innerHTML"] },
 };
