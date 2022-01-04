21c1cd0b3e23bccaa88dd4787b85251cd4d1840e
Alice Anglesjö <alice.anglesjo@hotmail.com>
2022-01-04 20:32:47 +0100

    add about

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import { Hero } from "../components/Hero";
+import { useEffect, useState } from "react";
+import groq from "groq";
+import client from "../client";
+import { Grid } from "../components/Grid";
+import { Card } from "../components/Card";
+import { TextBlock } from "../components/TextBlock";
+import { theme } from "../styles/styles";
+import { Loader } from "../components/Loader";
+
+export default function Abouy() {
+  return (
+    <div>
+      <h1>Om</h1>
+    </div>
+  );
+}
