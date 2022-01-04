eef7a0c1d24d4d87ebd34a5304199bad47b60ce6
Alice Anglesjö <alice.anglesjo@hotmail.com>
2022-01-04 22:56:19 +0100

    new plugon

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { GlobalStyle } from "../styles/styles";
 import Layout from "../components/Layout";
 import { theme } from "../styles/styles";
 import "./_app.css";
+import { MessengerChat } from "react-messenger-chat-plugin";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,29 @@ function MyApp({ Component, pageProps }) {
       <ThemeProvider theme={theme}>
         <Layout>
           <Component {...pageProps} />
+          <MessengerChat
+            pageId="734860390568287"
+            language="sv_SE"
+            themeColor={"#F2F3G2"}
+            height={24}
+            loggedInGreeting="Hello logged in user!"
+            loggedOutGreeting="Hello stranger!"
+            autoExpand={true}
+            debugMode={false}
+            onMessengerShow={() => {
+              console.log("onMessengerShow");
+            }}
+            onMessengerHide={() => {
+              console.log("onMessengerHide");
+            }}
+            onMessengerDialogShow={() => {
+              console.log("onMessengerDialogShow");
+            }}
+            onMessengerDialogHide={() => {
+              console.log("onMessengerDialogHide");
+            }}
+          />
+          ;
         </Layout>
       </ThemeProvider>
     </>
