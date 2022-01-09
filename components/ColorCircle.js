ce6b2c4cf9f2c8b0060b1c0a8d5f87d03f4c3732
Alice Anglesjö <alice.anglesjo@hotmail.com>
2022-01-09 15:44:25 +0100

    larger circle

--- components/ColorCircle.js
+++ components/ColorCircle.js
@@ -2,13 +2,13 @@ import { Breakpoints } from "../styles/styles";
 import styled from "styled-components";
 
 const Circle = styled.div(({ circleColor }) => ({
-  height: 11,
-  width: 11,
+  height: 10,
+  width: 10,
   display: "inline-block",
   borderRadius: "50%",
   marginRight: 4,
   backgroundColor: circleColor ? circleColor : "red",
-  [Breakpoints.Medium]: {
+  [Breakpoints.Large]: {
     height: 14,
     width: 14,
     marginRight: 6,
